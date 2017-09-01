bbb2466d1a723c9f203acd56b62c7a56566873de
Panchenko Ivanna <ivanna@cloudinit.nz>
2017-09-01 16:06:36 +1200

    another component created

--- out/production/TrainingSalesforce/aura/campingListItem/campingListItemController.js
+++ out/production/TrainingSalesforce/aura/campingListItem/campingListItemController.js
@@ -2,9 +2,7 @@
 	packItem  : function(component, event, helper) {
         var btnClicked = event.getSource();         // the button
         var btnMessage = btnClicked.get("v.label"); // the button's label
-        var a = component.get("v.item",true);
-            a.Packed__c = true;
-            component.set("v.item",a);
+        component.set("v.item.Packed__c", true);     // update our message
         btnClicked.set("v.disabled",true); //disabled button 
 	}
 })
---
+++ src/aura/CampingTesting/CampingTesting.app
@@ -0,0 +1,3 @@
+<aura:application extends="ltng:outApp" >
+    <c:campingListItem />
+</aura:application>
